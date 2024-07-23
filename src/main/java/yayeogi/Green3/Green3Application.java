2130b0c634007a9280984d65613f6dc4a394562b
21-manic-006g <wayz1@naver.com>
2024-07-23 13:00:26 +0900

    첫번째 커밋 입니다

---
+++ src/main/java/yayeogi/Green3/Green3Application.java
@@ -0,0 +1,13 @@
+package yayeogi.Green3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Green3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Green3Application.class, args);
+	}
+
+}
