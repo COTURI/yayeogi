4fb1cb4b2af0486ff8f78ead47c71b6c5c916331
21-manic-005g <rlatjddbs53@naver.com>
2024-08-02 10:03:11 +0900

    Hotels 아마데우스 호텔 api 사용 시도

--- src/main/java/yayeogi/Green3Application.java
+++ src/main/java/yayeogi/Green3Application.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class Green3Application {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(Green3Application.class, args);
 	}
 
