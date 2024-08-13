b994a37cca90beb29157d803d006eba48933bbcc
21-manic-005g <rlatjddbs53@naver.com>
2024-08-13 15:46:12 +0900

    Hotels 8월 13일까지

--- src/main/java/yayeogi/Green3/repository/HotelRepository.java
+++ src/main/java/yayeogi/Green3/repository/HotelRepository.java
@@ -4,14 +4,16 @@ import org.springframework.beans.PropertyValues;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yayeogi.Green3.DTO.HotelDTO;
 import yayeogi.Green3.entity.Hotel;
+import yayeogi.Green3.entity.HotelReview;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
     List<Hotel> findByCountryAndLocation(Integer country, Integer location);
     List<Hotel> findByCountry(Integer country);
     List<Hotel> findByLocation(Integer location);
     List<Hotel> findByHotelId(Integer hotelId);
-
+    List<Hotel> findByCityOrNameContaining(String query);
 }
 
